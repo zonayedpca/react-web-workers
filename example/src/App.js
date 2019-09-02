58e25658d8458badc6f90904d6964fa7a5fc9c0f
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-02 08:33:09 +0530

    Renaming the project

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-modern-library-boilerplate'
+import ExampleComponent from 'react-web-worker'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent text='Working with Web Workers in React' />
       </div>
     )
   }
